4a0e334628d64dfd6120b3099b2132620e8cf7fb
Amrit-786 <amritpalsingh9599123@gmail.com>
2024-05-09 22:50:23 +0530

    navbar completed

---
+++ Product/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Components/Navbar.jsx";
+
+
+const App=()=>{
+  return(
+    <div className="App">
+      <Navbar/>
+    </div>
+  )
+}
+export default App;
---
+++ Product/src/Components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+const home = () => {
+  return (
+    <div className='home-contanier'><Navbar/></div>
+  )
+}
+
+export default home;
