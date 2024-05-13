12cac235dbaad954891ca613c4405e947abd4f7b
Amrit-786 <amritpalsingh9599123@gmail.com>
2024-05-13 18:39:32 +0530

    landing page and course

--- Product/src/Components/Navbar.jsx
+++ Product/src/Components/Navbar.jsx
@@ -1,134 +1,314 @@
-import React from 'react';
-import logoImage from '../assets/pL.png'
+import logo from "../assets/logoImage.png";
+// import {Link} from 'react-router-dom';
 import {
-  Box,
-  Flex,
-  HStack,
-  IconButton,
-  useDisclosure,
-  Stack,
-  Link,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Image,
-  Heading,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Button,
+    Stack,
+    Collapse,
+    Icon,
+    Link,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
 
-const Links = [
-  { name: 'Certifications', submenu: [
-    { name: 'Product Manager Certification', path: '#' },
-    { name: 'Artificial Intelligence for Product Certification ', path: '#' },
-    { name: 'Product Leader Certification', path: '#' },
-    { name: 'Product Marketing Manager Certification', path: '#' },
-  ]},
-  { name: 'For Teams', submenu: [
-    { name: 'Custom Product Training', path: '#' },
-    { name: 'AI Training', path: '#' },
-  ]},
-  { name: 'Our Instructors', path: '#' },
-  { name: 'Resources', path: '#' },
-  { name: 'Events', submenu: [
-    { name: 'Free Daily Events', path: '#' },
-    { name: 'ProductCon Conferences', path: '#' },
-    { name: 'Sponsorships', path: '#' },
-  ]},
-];
-
-const NavLink = ({ name, path, submenu }) => (
-    <Box>
-      {submenu ? (
-        <Menu>
-          <MenuButton
-            as={Link}
-            px={4}
-            py={2}
-            rounded={'md'}
-            fontWeight={'semibold'}
-            fontSize={'xl'}
-            color={'Black'}
-            bg={'grey.400'}
-            _hover={{ color: 'black.400' }}
-            _expanded={{ color: 'black.600' }}
-          >
-            {name}
-          </MenuButton>
-          <MenuList bg={'gray.100'} border={'1px solid'} borderColor={'gray.300'}>
-            {submenu.map((item) => (
-              <MenuItem
-                key={item.name}
-                _hover={{ color: 'black', color: 'white' }}
-                px={4}
-                py={2}
-                rounded={'md'}
-              >
-                <Link href={item.path}>{item.name}</Link>
-              </MenuItem>
-            ))}
-          </MenuList>
-        </Menu>
-      ) : (
-        <Link
-          px={4}
-          py={2}
-          rounded={'md'}
-          fontWeight={'semibold'}
-          fontSize={'xl'}
-          color={'black'}
-        //   bg={'teal.600'}
-          _hover={{ bg: 'brown.500' }}
-          href={path}
-        >
-          {name}
-        </Link>
-      )}
-    </Box>
-  );
+  } from '@chakra-ui/react';
+  import {
+    HamburgerIcon,
+    CloseIcon,
+    ChevronDownIcon,
+    ChevronRightIcon,
+  } from '@chakra-ui/icons';
   
-  const Navbar = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  export default function Navbar() {
+    const { isOpen, onToggle } = useDisclosure();
   
     return (
-      <Box bg={'white'} boxShadow={'md'} py={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'} maxW={'1200px'} mx={'auto'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon color={'black'} /> : <HamburgerIcon color={'red'} />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-          {/* <Heading as={'h1'} size={'lg'} color={'green.500'}>
-            Product School
-          </Heading> */}
-         <Image src={logoImage} alt="Logo" boxSize="100px" style={{ maxWidth: '100%', height: 'auto' }} /> 
-            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-              {Links.map(({ name, path, submenu }) => (
-                <NavLink key={name} name={name} path={path} submenu={submenu} />
-              ))}
-            </HStack>
-          </HStack>
-          <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-            <NavLink name={'Sign In'} path={'#'} />
-          </HStack>
-        </Flex>
-
-
-      {isOpen ? (
-        <Box pb={4} display={{ md: 'none' }}>
-          <Stack as={'nav'} spacing={4}>
-            {Links.map(({ name, path, submenu }) => (
-              <NavLink key={name} name={name} path={path} submenu={submenu} />
-            ))}
-            <NavLink name={'Sign In'} path={'#'} />
+      <Box position="sticky" top="0" zIndex="sticky">
+        <Flex
+          bg={useColorModeValue('white', 'gray.800')}
+          color={useColorModeValue('gray.600', 'white')}
+          minH={'80px'}
+          py={{ base: 4, md:2 }}
+          px={{ base: 6, md:4 }}
+          borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.900')}
+          align={'center'}>
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            ml={{ base: -2 }}
+            display={{ base: 'flex', md: 'none' }}>
+            <IconButton
+              onClick={onToggle}
+              icon={
+                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+              }
+              variant={'ghost'}
+              aria-label={'Toggle Navigation'}
+            />
+          </Flex>
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <Box>
+            <img src={logo} alt="Logo" height={1}   width={65} mt="20px" mb="20px" /> 
+          </Box>
+  
+            <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+              <DesktopNav />
+            </Flex>
+          </Flex>
+  
+          <Stack
+            flex={{ base: 1, md: 0 }}
+            justify={'flex-end'}
+            direction={'row'}
+            spacing={6}>
+            <Button
+              as={'a'}
+             
+              fontSize={'l'}
+              fontWeight={600}
+              color={'#103869'}
+             variant={'link'}
+              href={'#'}>
+              LogIn
+            </Button>
+            <Button
+              as={'a'}
+              display={{ base: 'none', md: 'inline-flex' }}
+              fontSize={'sm'}
+              fontWeight={600}
+              href={'#'}
+              color={'white'}
+              bg={'#103869'}
+              _hover={{ transform: 'scale(1.05)',
+              }}>
+              Sign Up
+            </Button>
           </Stack>
-        </Box>
-      ) : null}
-    </Box>
-  );
-};
+        </Flex>
+  
+        <Collapse in={isOpen} animateOpacity>
+          <MobileNav />
+        </Collapse>
+      </Box>
+    );
+  }
+  
+  const DesktopNav = () => {
+    const linkColor = useColorModeValue('gray.600', 'gray.200');
+    const linkHoverColor = useColorModeValue('gray.800', 'white');
+    const popoverContentBgColor = useColorModeValue('white', 'gray.800');
+  
+    return (
+      <Stack direction={'row'} spacing={4}>
+        {NAV_ITEMS.map((navItem) => (
+          <Box key={navItem.label}>
+            <Popover trigger={'hover'} placement={'bottom-start'}>
+              <PopoverTrigger>
+                <Link
+                  p={2}
+                  href={navItem.href ?? '#'}
+                  fontSize={'sm'}
+                  fontWeight={500}
+                  color={linkColor}
+                  _hover={{
+                    textDecoration: 'none',
+                    color: linkHoverColor,
+                  }}>
+                  {navItem.label}
+                </Link>
+              </PopoverTrigger>
+  
+              {navItem.children && (
+                <PopoverContent
+                  border={0}
+                  boxShadow={'xl'}
+                  bg={popoverContentBgColor}
+                  p={4}
+                  rounded={'xl'}
+                  minW={'sm'}>
+                  <Stack>
+                    {navItem.children.map((child) => (
+                      <DesktopSubNav key={child.label} {...child} />
+                    ))}
+                  </Stack>
+                </PopoverContent>
+              )}
+            </Popover>
+          </Box>
+        ))}
+      </Stack>
+    );
+  };
+  
+  const DesktopSubNav = ({ label, href, subLabel }) => {
+    return (
+      <Link
+        href={href}
+        role={'group'}
+        display={'block'}
+        p={2}
+        rounded={'md'}
+        _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+        <Stack direction={'row'} align={'center'}>
+          <Box>
+            <Text
+              transition={'all .3s ease'}
+              _groupHover={{ color: 'pink.400' }}
+              fontWeight={500}>
+              {label}
+            </Text>
+            <Text fontSize={'sm'}>{subLabel}</Text>
+          </Box>
+          <Flex
+            transition={'all .3s ease'}
+            transform={'translateX(-10px)'}
+            opacity={0}
+            _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+            justify={'flex-end'}
+            align={'center'}
+            flex={1}>
+            <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+          </Flex>
+        </Stack>
+      </Link>
+    );
+  };
+  
+  const MobileNav = () => {
+    return (
+      <Stack
+        bg={useColorModeValue('white', 'gray.800')}
+        p={4}
+        display={{ md: 'none' }}>
+        {NAV_ITEMS.map((navItem) => (
+          <MobileNavItem key={navItem.label} {...navItem} />
+        ))}
+      </Stack>
+    );
+  };
+  
+  const MobileNavItem = ({ label, children, href }) => {
+    const { isOpen, onToggle } = useDisclosure();
+  
+    return (
+      <Stack spacing={4} onClick={children && onToggle}>
+        <Flex
+          py={2}
+          as={Link}
+          href={href ?? '#'}
+          justify={'space-between'}
+          align={'center'}
+          _hover={{
+            textDecoration: 'none',
+          }}>
+          <Text
+            fontWeight={600}
+            color={useColorModeValue('gray.600', 'gray.200')}>
+            {label}
+          </Text>
+          {children && (
+            <Icon
+              as={ChevronDownIcon}
+              transition={'all .25s ease-in-out'}
+              transform={isOpen ? 'rotate(180deg)' : ''}
+              w={6}
+              h={6}
+            />
+          )}
+        </Flex>
+  
+        <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+          <Stack
+            mt={2}
+            pl={4}
+            borderLeft={1}
+            borderStyle={'solid'}
+            borderColor={useColorModeValue('gray.200', 'gray.700')}
+            align={'start'}>
+            {children &&
+              children.map((child) => (
+                <Link key={child.label} py={2} href={child.href}>
+                  {child.label}
+                </Link>
+              ))}
+          </Stack>
+        </Collapse>
+      </Stack>
+    );
+  };
+  
+ 
+  
+  const NAV_ITEMS= [
+    {
+      label: 'Certifications',
+      children: [
+        {
+          label: 'Product Manager Certification',
+          subLabel: 'Get your first Product Management job',
+          href: '#',
+        },
+        {
+            label: 'Product Leader Certification',
+            subLabel: 'Get your next Product Management promotion',
+            href: '#',
+          },
+        {
+            label: 'Product Marketing Manager Certification',
+            subLabel: 'Bring your products to market successfully',
+            href: '#',
+          },
+      ],
+    },
+    {
+      label: 'Courses',
+      href: 'http://localhost:5173/courses',
+      // children: [
+      //   {
+      //     label: 'Product Management Courses',
+      //     href: '/courses/product-management',
+      //   },
+      //   {
+      //     label: 'AI Training',
+      //     href: '#',
+      //   },
+      // ],
+    },
+    {
+      label: 'Our Instructors',
+      href: '#',
+    },
+    {
+      label: 'Resources',
+      href: '#',
+    },
 
-export default Navbar;
+    {
+        label: 'Events',
+        children: [
+          {
+            label: 'Free Daily Events',
+            href: '#',
+          },
+          {
+            label: 'ProductCon Conferences',
+            href: '#',
+          },
+          {
+            label: 'Sponsorships',
+            href: '#',
+          },
+        ],
+      },
+
+
+
+    
+  ];
---
+++ Product/src/Components/course.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Image, Badge, Text, Button, Grid, HStack, useColorModeValue } from '@chakra-ui/react';
+import CartPage from '../cartPage';
+
+const CourseCard = ({}) => {
+  const [courses, setCourses] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage  = 20;
+
+  useEffect(() => {
+    fetch('https://course-api-jo5j.onrender.com/courses')
+      .then((response) => response.json())
+      .then((data) => setCourses(data))
+      .catch((error) => console.error('Error fetching courses:', error));
+  }, []);
+
+  const handleAddToCart = (course) => {
+    setCart((prevCart) => [...prevCart, course]);
+  };
+
+  const totalPages = Math.ceil(courses.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage; 
+  const endIndex = startIndex + itemsPerPage;
+  const currentCourses = courses.slice(startIndex, endIndex);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <Button key={i}
+          onClick={() => handlePageChange(i)}
+          colorScheme={currentPage === i ? 'teal' : 'blue'}
+        >
+          {i}
+        </Button>
+      );
+    }
+    return (
+      <HStack mt={4} spacing={4} justifyContent="center">{pageNumbers}</HStack>
+    );
+  };
+
+  return (
+    <>
+    <Box>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        {currentCourses.map((course) => (
+        //    <CartPage cart={cart} />
+          <Box
+           
+            key={course.Title}
+            bg={useColorModeValue('white', 'gray.800')}
+            maxW="sm"
+            borderWidth="1px"
+            rounded="lg"
+            shadow="lg"
+            position="relative"
+          >
+            <Image src={course.image} alt={`${course.Title} Thumbnail`} roundedTop="lg" />
+
+            <Box p="6">
+              <Box d="flex" alignItems="baseline">
+                <Badge rounded="full" px="2" colorScheme="teal">
+                  New
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  {course.Rating} &bull; {course['instructor-name']}
+                </Box>
+              </Box>
+
+              <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+                {course.Title}
+              </Box>
+
+              <Text mt="2" noOfLines={2}>
+                {course.Description}
+              </Text>
+
+              <Box>
+                <Box as="span" color="gray.600" fontSize="sm">
+                  {course.hours}
+                </Box>
+              </Box>
+
+              <Button mt="4" colorScheme="teal" variant="outline" onClick={() => handleAddToCart(course)}>
+                Add to Cart
+              </Button>
+            </Box>
+          </Box>
+        ))}
+      </Grid>
+      {renderPagination()}
+      
+    </Box>
+   
+   
+    </>
+  );
+};
+
+export default CourseCard;
